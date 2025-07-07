8de23e07f77ba62bc108e1be48006d6f39d422e7
JA Proger <134533485+ja-proger@users.noreply.github.com>
2025-07-07 22:31:30 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,13 +5,15 @@ pid=$!
 spinner='-/|\'
 i=0
 
-while kill 2>/dev/null; do
+while kill -0 2>/dev/null; do
+    i=( () )
+    printf "\r"
+done
 
-end
-
-$status
+$status=pid
 
 if kill -eq 0; then
+    printf ""
 else
     printf "\033[1;33mSomething goes wrong!\033[0m"
 fi
